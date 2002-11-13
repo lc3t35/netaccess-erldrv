0230d6f78b4a4084002843d75b0236804851e55e
vances <vances@d5e56d7b-e937-0410-bc91-e1df7374187b>
2002-11-13 15:10:44 +0000

    implemented functions to set/query time slot interchange
    	implemented portable (32 & 64 bit) method of handling thread references

--- trunk/src/netaccess.erl
+++ trunk/src/netaccess.erl
@@ -25,9 +25,7 @@
 -export([select_board/2,boot/2,enable_management_chan/1,
 			reset_board/1,get_version/1, get_driver_info/1]).
 -export([set_hardware/2, set_hardware/3, set_hardware/4, req_hw_status/1]).
-
-% test
--export([mergeopts/2]).
+-export([set_tsi/3, req_tsi_status/1]).
 
 
 %% ioctl commands
@@ -52,18 +50,18 @@
 %% defines for get_driver_info/2
 %%
 -define(DriverInfoMask,
-		<<BoardType:?SIZEINT/?ENDIANESS-signed-integer,
-		HangUpOnRedAlarm:?SIZEINT/?ENDIANESS-signed-integer,
-		FlowControlBoard:?SIZEINT/?ENDIANESS-signed-integer,
-		FlowControlWsrv:?SIZEINT/?ENDIANESS-signed-integer,
-		FlowControlRsrv:?SIZEINT/?ENDIANESS-signed-integer,
-		HDrops:?SIZEINT/?ENDIANESS-signed-integer,
-		SDrops:?SIZEINT/?ENDIANESS-signed-integer,
-		TxMsgSize:?SIZEINT/?ENDIANESS-signed-integer,
-		RxMsgSize:?SIZEINT/?ENDIANESS-signed-integer,
-		TxNumBufs:?SIZEUSHORT/?ENDIANESS-unsigned-integer,
-		RxNumBufs:?SIZEUSHORT/?ENDIANESS-unsigned-integer,
-		MaxDataChannels:?SIZEUINT/?ENDIANESS-unsigned-integer>>).
+		<<BoardType:?SIZEINT/?ENDIANESS-signed-integer-unit:8,
+		HangUpOnRedAlarm:?SIZEINT/?ENDIANESS-signed-integer-unit:8,
+		FlowControlBoard:?SIZEINT/?ENDIANESS-signed-integer-unit:8,
+		FlowControlWsrv:?SIZEINT/?ENDIANESS-signed-integer-unit:8,
+		FlowControlRsrv:?SIZEINT/?ENDIANESS-signed-integer-unit:8,
+		HDrops:?SIZEINT/?ENDIANESS-signed-integer-unit:8,
+		SDrops:?SIZEINT/?ENDIANESS-signed-integer-unit:8,
+		TxMsgSize:?SIZEINT/?ENDIANESS-signed-integer-unit:8,
+		RxMsgSize:?SIZEINT/?ENDIANESS-signed-integer-unit:8,
+		TxNumBufs:?SIZEUSHORT/?ENDIANESS-unsigned-integer-unit:8,
+		RxNumBufs:?SIZEUSHORT/?ENDIANESS-unsigned-integer-unit:8,
+		MaxDataChannels:?SIZEUINT/?ENDIANESS-unsigned-integer-unit:8>>).
 -define(DriverInfoTerms,
 		[{board_type, BoardType}, {hangup_on_red_alarm, HangUpOnRedAlarm},
 		{flow_control_board, FlowControlBoard},
@@ -81,10 +79,54 @@
 		<<0, LapdId:?PRIu8bit, MsgType:?PRIu8bit, L4Ref:?PRIu16bit,
 		CallRef:?PRIu16bit, Lli:?PRIu16bit>>).
 -define(L3L4_Mask(LapdId, MsgType, L4Ref, CallRef, BChan,
-		Iface, BChanMask, Lli, DataChan),
+		Iface, BChanMask, Lli, DataChan, Data),
 		<<LapdId:?PRIu8bit, MsgType:?PRIu8bit, L4Ref:?PRIu16bit,
 		CallRef:?PRIu16bit, BChan:?PRIu8bit, Iface:?PRIu8bit,
-		 BChanMask:?PRIu32bit, Lli:?PRIu16bit, DataChan:?PRIu16bit>>).
+		BChanMask:?PRIu32bit, Lli:?PRIu16bit, DataChan:?PRIu16bit,
+		Data/binary>>).
+
+%%
+%% error in response to L4L3mXXXXX
+%%
+-define(L3L4mERROR, 16#20). 
+-define(L3L4mErrorMsg(X), element(2, element(2, lists:keysearch(X, 1,
+		[{0, no_erorr}, {1, lapdid_out_of_range},
+		{2, lapdid_not_established}, {3, invalid_called_number},
+		{4, no_crv_available}, {5, no_crstruct_available},
+		{6, call_ref_error}, {7, invalid_b_channel},
+		{8, b_chanel_restarting}, {9, b_chanel_oos}, 
+		{10, invalid_call_type}, {11, invalid_conn_type},
+		{12, protocol_not_disabled}, {13, invalid_hdlc_maping},
+		{14, invalid_data_queue}, {15, invalid_comand_args},
+		{16, invalid_msg_for_state}, {17, data_packet_lost},
+		{18, pm_not_esf}, {19, invalid_interface},
+		{20, b_channel_inuse}, {21, invalid_lli},
+		{22, vc_table_full}, {23, lli_not_found}, {24, blocked},
+		{25, no_hardware}, {26, invalid_spid_len}, {27, non_nfas},
+		{28, invalid_state}, {29, service_not_offered},
+		{30, dchan_temp_unavail}, {31, too_many_q931_stacks},
+		{32, service_not_config}, {33, data_interface_required},
+		{34, data_interface_invalid}, {35, sym_mode_not_supported},
+		{36, invalid_bufsz}, {37, bond_chan_not_cnfg},
+		{38, bond_chan_bit_conflict}, {39, bond_wrong_spyder_chip},
+		{40, bond_too_many_channels}, {41, bond_dup_addon_chan},
+		{42, dchan_odd_pointer_error}, {43, dchan_too_few_buffers},
+		{44, dchan_too_many_buffers}, {45, dchan_give_take_nonzero},
+		{46, dchan_zero_rxbuf_len}, {47, sym_mode_required},
+		{48, dlci_manditory}, {49, chan_kbit_rate_bad},
+		{50, invalid_mem_size}, {51, not_enough_memory},
+		{52, tx_buffer_misaligned}, {53, x_buffer_misaligned},
+		{54, too_many_dlcis}, {55, bond_bad_state}, {56, spid_rejected},
+		{57, tei_ident_remove_req}, {58, spid2_rejected},
+		{59, invalid_smi_msgid}, {60, invalid_clock_mode},
+		{61, no_overflow_queue}, {62, too_many_cas_dest},
+		{63, segment_too_large}, {64, segment_message_expected},
+		{65, segment_message_invalid}, {66, segment_timer_expired},
+		{67, invalid_download_msg}, {68, protocol_disabled},
+		{69, invalid_variant}, {70, too_many_links}, {71, too_many_headers},
+		{72, fatal_error}, {73, hot_swap_extraction},
+		{74, dchan_out_of_range}])))).
+
 
 %%
 %% defines for set_hardware/2 and req_hw_status/1
@@ -93,8 +135,7 @@
 -define(L4L3mREQ_HW_STATUS, 16#A8). 
 -define(L3L4mHARDWARE_STATUS, 16#24). 
 -define(PRI_HARDWARE_DATA,
-		<<_:?PRIu8bit, ?L3L4mHARDWARE_STATUS:?PRIu8bit,
-		_:(14*8), HardwareBin:12/binary,
+		<<HardwareBin:12/binary,
 		LineBins:(16*?PRI_MAX_LINES)/binary,
 		CsuBin:(?PRI_MAX_LINES)/binary, _/binary>>).
 -define(HardwareMask, <<Clocking:?PRIu8bit, Clocking2:?PRIu8bit,
@@ -135,6 +176,85 @@
 		{bri_loop,0}, {filter_unsolicited,0}, {filter_yellow,0},
 		{framing,0}, {integrate_alarms,0}, {line_code,0},
 		{line_length,0}, {line_type,0}, {pm_mode,0}, {term,0}]).
+
+
+%%
+%% defines for set_tsi/2 and req_tsi/1
+%%
+-define(L4L3mSET_TSI, 16#A9). 
+-define(L4L3mREQ_TSI_STATUS, 16#D4). 
+-define(L3L4mTSI_STATUS, 16#4A). 
+-define(PRI_MAX_SET_TSI, 120).
+-define(PRItsiGRANULARITY_64K, 16#00).
+-define(PRItsiGRANULARITY_32K, 16#01).
+-define(PRItsiGRANULARITY_16K, 16#02).
+-define(PRItsiGRANULARITY_8K, 16#03).
+-define(PRItsiDSi0, 16#0000).
+-define(PRItsiDSi1, 16#0100).
+-define(PRItsiDSi2, 16#0200).
+-define(PRItsiDSi3, 16#0300).
+-define(PRItsiDSi4, 16#0400).
+-define(PRItsiDSi5, 16#0500).
+-define(PRItsiDSi6, 16#0600).
+-define(PRItsiDSi7, 16#0700).
+-define(PRItsiDSo0, 16#0800).
+-define(PRItsiDSo1, 16#0900).
+-define(PRItsiDSo2, 16#0a00).
+-define(PRItsiDSo3, 16#0b00).
+-define(PRItsiDSo4, 16#0c00).
+-define(PRItsiDSo5, 16#0d00).
+-define(PRItsiDSo6, 16#0e00).
+-define(PRItsiDSo7, 16#0f00).
+-define(PRItsiLINE_A, 16#1000).
+-define(PRItsiLINE_B, 16#1100).
+-define(PRItsiLINE_C, 16#1200).
+-define(PRItsiLINE_D, 16#1300).
+-define(PRItsiLINE_E, 16#1400).
+-define(PRItsiLINE_F, 16#1500).
+-define(PRItsiLINE_G, 16#1600).
+-define(PRItsiLINE_H, 16#1700).
+-define(PRItsiHDLC_0, 16#2000).
+-define(PRItsiHDLC_1, 16#2100).
+-define(PRItsiHDLC_2, 16#2200).
+-define(PRItsiHDLC_3, 16#2300).
+-define(PRItsiHDLC_4, 16#2400).
+-define(PRItsiHDLC_5, 16#2500).
+-define(PRItsiHDLC_6, 16#2600).
+-define(PRItsiHDLC_7, 16#2700).
+-define(PRItsiHDLC_8, 16#2800).
+-define(PRItsiHDLC_9, 16#2900).
+-define(PRItsiHDLC_10, 16#2a00).
+-define(PRItsiHDLC_11, 16#2b00).
+-define(PRItsiHDLC_12, 16#2c00).
+-define(PRItsiHDLC_13, 16#2d00).
+-define(PRItsiHDLC_14, 16#2e00).
+-define(PRItsiHDLC_15, 16#2f00).
+-define(PRItsiHDLC_16, 16#3000).
+-define(PRItsiMODEM_0, 16#6000).
+-define(PRItsiMODEM_1, 16#6100).
+-define(PRItsiDISABLE, 16#4000).
+-define(PRItsiPATTERN, 16#5000).
+-define(PRItsiDSP_0, 16#7000).
+-define(PRItsiDSP_1, 16#7100).
+-define(PRItsiDSP_2, 16#7200).
+-define(PRItsiDSP_3, 16#7300).
+-define(PRItsiDSP_4, 16#7400).
+-define(PRItsiDSP_5, 16#7500).
+-define(PRItsiDSP_6, 16#7600).
+-define(PRItsiDSP_7, 16#7700).
+-define(PRItsiDSi(X), (X bsl 8)).
+-define(PRItsiDSo(X), ((X + 8) bsl 8)).
+-define(PRItsiLINE(X), 	(16#1000 bor (X bsl 8)).
+-define(PRItsiHDLC(X), 	(16#2000 bor (X bsl 8)).
+-define(PRItsiMODEM(X), (16#6000 bor (X bsl 8)).
+-define(PRItsiDSP(X), 	(16#7000 bor (X bsl 8)).
+-define(PRItsiCTd(X), 	(16#8000 bor (X bsl 10)).
+-define(PRI_TSI_DATA,
+		<<NumMappings:?PRIu16bit, Granularity:?PRIu8bit, Last:?PRIu8bit,
+		TsiMapBins/binary>>).
+-define(TsiMapMask, <<Destination:?PRIu16bit, Source:?PRIu16bit>>).
+-define(TsiMapTerms, [{destination, Destination}, {source, Source}]).
+
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -247,7 +367,7 @@ get_version(Port) ->
 %%
 %% get driver information from an open netaccess board
 %%
-%% returns {ok, <<DriverInfo>>} or {error, Reason}
+%% returns {ok, [} or {error, Reason}
 %%
 get_driver_info(Port) ->
 	case do_ioctl({ioctl, ?GET_DRIVER_INFO, [], Port}) of
@@ -256,6 +376,11 @@ get_driver_info(Port) ->
 	end.
 	
 
+%%
+%% set hardware settings on board such as clocking, framing
+%%
+%% returns {ok, 
+%% 
 
 % may be called without line settings and CSU flags, we'll use defaults
 set_hardware(Port, HardwareSettings) ->
@@ -287,25 +412,17 @@ set_hardware(Port, HardwareSettings, LineSettings, CsuFlags)
 	LineDefaults = ?LineDefaults,
 	HardwareMerged = lists:keysort(1, mergeopts(HardwareSettings,
 			HardwareDefaults)),
+	?HardwareTerms = HardwareMerged,
+	HardwareBin = ?HardwareMask,
 	LineMerged = [lists:keysort(1, mergeopts(A, B)) ||
 			A <- LineSettings, B <- [LineDefaults]],
-	do_set_hardware(Port, HardwareMerged, LineMerged, CsuFlags).
-	
-%%
-%% internal function to set the hardware options
-%%
-do_set_hardware(Port, ?HardwareTerms, LineSettings, CsuFlags) ->
-	HardwareBin = ?HardwareMask,
 	MakeLineBin = fun(?LineTerms) -> ?LineMask end,
 	LineBins = [MakeLineBin(A) || A <- LineSettings],
 	CsuBin = list_to_binary(CsuFlags),
-	L4L3_Bin = ?L4L3_Mask(0, ?L4L3mSET_HARDWARE, 0, 0, 0),
+	L4_Ref = 16#FFFF,
+	L4L3_Bin = ?L4L3_Mask(0, ?L4L3mSET_HARDWARE, L4_Ref, 0, 0),
 	L4_to_L3_struct = concat_binary([L4L3_Bin, HardwareBin, LineBins, CsuBin]),
-	port_command(Port, L4_to_L3_struct),
-	receive 
-		{Port, {error, Reason}} -> {error, Reason}
-		after 200 -> {ok, done}
-	end.
+	do_call({smi, Port, L4_Ref, L4_to_L3_struct}).
 
 
 %%
@@ -314,24 +431,73 @@ do_set_hardware(Port, ?HardwareTerms, LineSettings, CsuFlags) ->
 %% returns {ok, [HardwareTerms], [[LineTerms]|...], CsuTerms}
 %%
 req_hw_status(Port) ->
-	L4L3_Bin = ?L4L3_Mask(0, ?L4L3mREQ_HW_STATUS, 0, 0, 0),
+	L4L3_Bin = ?L4L3_Mask(0, ?L4L3mREQ_HW_STATUS, 16#FFFF, 0, 0),
 	port_command(Port, L4L3_Bin),
 	receive 
 		{Port, {error, Reason}} -> {error, Reason};
-		{Port, {?PRI_HARDWARE_DATA, DataBin}} ->
+		{Port, {?L3L4_Mask(_LapdId, ?L3L4mHARDWARE_STATUS, _L4Ref, _CallRef,
+				_BChan, _IFace, _BChanMask, _Lli, _DataChan, Rest), _DataBin}} ->
+			?PRI_HARDWARE_DATA = Rest,
 			?HardwareMask = HardwareBin,
-			{ok, ?HardwareTerms, req_hw_status1(LineBins,
+			{ok, ?HardwareTerms, req_hw_status(LineBins,
 					size(LineBins) div ?PRI_MAX_LINES, []),
 					binary_to_list(CsuBin)}; 
 		{Port, {error, Reason}} -> {error, Reason}
-		after 200 -> {error, timeout}
+	after
+		100 -> {error, timeout}
 	end.
-req_hw_status1(<<>>, LineBinSize, LineTerms) -> LineTerms;
-req_hw_status1(LineBins, LineBinSize, LineTerms) ->
+req_hw_status(<<>>, LineBinSize, LineTerms) -> LineTerms;
+req_hw_status(LineBins, LineBinSize, LineTerms) ->
 	{?LineMask, Rest} = split_binary(LineBins, LineBinSize),
-	req_hw_status1(Rest, LineBinSize, LineTerms ++ ?LineTerms).
+	req_hw_status(Rest, LineBinSize, LineTerms ++ ?LineTerms).
 	
 	
+%%
+%% create timeslot mappings
+%%
+set_tsi(Port, Granularity, TsiMapTerms) ->
+	set_tsi(Port, length(TsiMapTerms), Granularity, TsiMapTerms, <<>>).
+set_tsi(Port, NumMappings, Granularity, [], TsiMapBins) ->
+	L4L3_Bin = ?L4L3_Mask(0, ?L4L3mSET_TSI, 16#FFFF, 0, 0),
+	Last = 0,
+	L4_to_L3_struct = concat_binary([L4L3_Bin, ?PRI_TSI_DATA]),
+	port_command(Port, L4_to_L3_struct),
+	receive 
+		{Port, {error, Reason}} -> {error, Reason};
+		{Port, {?L3L4_Mask(_LapdId, ?L3L4mERROR, _L4Ref, _CallRef,
+				_BChan, _IFace, _BChanMask, _Lli, _DataChan, Rest), _DataBin}} ->
+			<<Error:?PRIu8bit, _/binary>> = Rest,
+			{error, ?L3L4mErrorMsg(Error)}
+	after
+		100 -> {ok, done}
+	end;
+set_tsi(Port, NumMappings, Granularity, [?TsiMapTerms | T], TsiMapBins) ->
+	set_tsi(Port, NumMappings, Granularity, T,
+			concat_binary([TsiMapBins, ?TsiMapMask])).
+
+
+%%
+%% query the timeslot mappings
+%%
+req_tsi_status(Port) ->
+	L4L3_Bin = ?L4L3_Mask(0, ?L4L3mREQ_TSI_STATUS, 16#FFFF, 0, 0),
+	port_command(Port, L4L3_Bin),
+	receive 
+		{Port, {error, Reason}} -> {error, Reason};
+		{Port, {?L3L4_Mask(_LapdId, ?L3L4mTSI_STATUS, _L4Ref, _CallRef,
+				_BChan, _IFace, _BChanMask, _Lli, _DataChan, Rest), _DataBin}} ->
+			?PRI_TSI_DATA = Rest,
+			{ok, {num_mappings, NumMappings},
+					{granularity, Granularity}, {last, Last},
+					req_tsi_status(TsiMapBins, NumMappings, [])};
+		{Port, {error, Reason}} -> {error, Reason}
+	after
+		100 -> {error, timeout}
+	end.
+req_tsi_status(TsiMapBins, 0, TsiTerms) -> TsiTerms;
+req_tsi_status(TsiMapBins, NumMaps, TsiTerms) ->
+	{?TsiMapMask, Rest} = split_binary(TsiMapBins, 4),
+	req_tsi_status(Rest, NumMaps - 1, TsiTerms ++ ?TsiMapTerms).
 	
 
 %%----------------------------------------------------------------------
@@ -361,7 +527,7 @@ handle_call({open, Board}, {Pid, _Tag}, State) ->
 		Port when port(Port) ->
 			case catch erlang:port_connect(Port, Pid) of
 				true ->
-					unlink(Port),
+					% unlink(Port),
 					Reply = {ok, Port};
 				{'EXIT', Reason} ->
 					Reply = {error, Reason}
@@ -390,6 +556,12 @@ handle_call({ioctl, Operation, Data, Port}, _From, State) ->
 			Reply = {ok, Ref}
 	end,
 	{reply, Reply, State};
+
+
+%% send an SMI message to the board
+handle_call({smi, Port, L4_Ref, L4L3_Msg}, From, State) ->
+	erlang:port_command(Port, L4L3_Msg),
+	{noreply, State};
 
 
 %% shutdown the netaccess server
@@ -442,7 +614,8 @@ do_ioctl({ioctl, Operation, Data, Port}, Timeout) ->
 		{ok, Ref} -> 
 			receive
 				{Port, Ref, Result} -> Result
-				after Timeout ->
+			after
+				Timeout ->
 					do_cast({ioctl, ?CANCEL_ASYNC, Ref, Port}),
 					{error, timeout}
 			end
