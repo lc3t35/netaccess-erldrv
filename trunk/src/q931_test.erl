066656994713b0b0946577469fcff65483f4b630
vances <vances@d5e56d7b-e937-0410-bc91-e1df7374187b>
2004-08-13 00:33:47 +0000

    Changed record names to align with SMI documentation.

--- trunk/src/q931_test.erl
+++ trunk/src/q931_test.erl
@@ -19,21 +19,30 @@
 
 -export([start/2]).
 
-%% @spec (LapdId::integer(), DceDte::integer()) -> void()
+%% @spec (LapdId::integer(), Side) -> void()
+%%		Side = user | network | symmetric
 %%
 %% @doc Start the test.
 %%
 start(LapdId, Side) ->
 	{ok, Channel} = netaccess:open(),
 	ok = netaccess:select_board(Channel, 0),
- 	L1 = #level1_cnfg{l1_mode = ?IISDNl1modHDLC},
- 	L2Parms = #l2_lap_params{
- 			mode = ?IISDNl2modLAP_D, 
- 			dce_dte = Side},
+ 	L1 = #level1{l1_mode = ?IISDNl1modHDLC},
+	case Side of
+		user ->
+			L2Parms = #l2_lap_params{dce_dte = ?IISDNdirUSER_SIDE},
+			Emul = 0;
+		network ->
+			L2Parms = #l2_lap_params{dce_dte = ?IISDNdirNETWORK_SIDE},
+			Emul = 1;
+		symmetric ->
+			L2Parms = #l2_lap_params{dce_dte = ?IISDNdirSYMMETRIC},
+			Emul = 0
+	end,
  	D = #data_interface{enable = 1},
- 	L2 = #level2_cnfg{par = L2Parms, data_interface = D},
-	Q = #q931_cnfg{variant = ?IISDNvarNATL_ISDN_2},
- 	L3 = #level3_cnfg{l3_mode = ?IISDNl3modQ931},
+ 	L2 = #level2{par = L2Parms, data_interface = D},
+	Q = #q931{variant = ?IISDNvarNATL_ISDN_2, net_side_emul = Emul},
+ 	L3 = #level3{l3_mode = ?IISDNl3modQ931, cnfg = Q},
 	ProtoData = #ena_proto_data{level1 = L1, level2 = L2, level3 = L3},
 	netaccess:enable_protocol(Channel, LapdId, ProtoData),
 	loop().
