6b5554538fffc0cab25c24de37a813a8cdb3fae9
vances <vances@d5e56d7b-e937-0410-bc91-e1df7374187b>
2004-09-03 22:02:40 +0000

    Changed supervisor in lapd_data example application to create 	"temporary" workers.

--- examples/lapd_data/lapd_sup.erl
+++ examples/lapd_data/lapd_sup.erl
@@ -5,7 +5,7 @@
 init([ServerName, DeviceName, BoardNumber, Timer, LapdIds]) ->
 	BoardStartArgs = [{local, ServerName}, DeviceName, BoardNumber],
 	BoardStartFunc = {netaccess, start_link, BoardStartArgs},
-	BoardChildSpec = {netaccess, BoardStartFunc, permanent,
+	BoardChildSpec = {netaccess, BoardStartFunc, temporary,
 			4000, worker, [netaccess]},
 	FsmChildSpecs = init_fsms(ServerName, Timer, LapdIds, []),
 	{ok, {{one_for_one, 10, 60}, [BoardChildSpec] ++ FsmChildSpecs}}.
@@ -15,7 +15,7 @@ init_fsms(ServerName, Timer, [], FsmChildSpecs) ->
 init_fsms(ServerName, Timer, [LapdId|T], FsmChildSpecs) ->
    FsmStartArgs = [lapd_fsm, [ServerName, LapdId, Timer], []],
    FsmStartFunc = {netaccess_fsm, start_link, FsmStartArgs},
-   FsmChildSpec = {LapdId, FsmStartFunc, permanent,
+   FsmChildSpec = {LapdId, FsmStartFunc, temporary,
 		4000, worker, [netaccess_fsm, lapd_fsm]},
 	init_fsms(ServerName, Timer, T, FsmChildSpecs ++ [FsmChildSpec]).
 
