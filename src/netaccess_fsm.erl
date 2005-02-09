399740dc482c1831c2f37d2d6f9cfa2053bc9f0d
vances <vances@d5e56d7b-e937-0410-bc91-e1df7374187b>
2005-02-09 01:36:28 +0000

    Added L3L4mERROR clause to netaccess_fsm behaviour. 	Added macros fro rate adaptation fill flags.

--- src/netaccess_fsm.erl
+++ src/netaccess_fsm.erl
@@ -227,6 +227,21 @@ handle_sync_event(Event, From, statename, {Module, StateName, StateData}) ->
 
 
 %% @hidden
+%% an L3L4mERROR message arrived from the driver
+handle_info({Port, {'L3L4m', CtrlBin, _DataBin}}, statename, {Module, StateName, StateData}) 
+			when is_binary(CtrlBin), size(CtrlBin) > 0 ->
+	L3L4_rec = iisdn:l3_to_l4(CtrlBin),
+	case Module:StateName({Port, L3L4_rec}, StateData) of
+		{next_state, NextStateName, NewStateData} ->
+			{next_state, statename, {Module, NextStateName, NewStateData}};
+		{next_state, NextStateName, NewStateData, Timeout} ->
+			{next_state, statename, {Module, NextStateName, NewStateData}, Timeout};
+		{stop, Reason, NewStateData} ->
+			{stop, Reason, {Module, StateName, NewStateData}};
+		Other ->
+			Other
+	end;
+%% @hidden
 %% an L3L4 SMI control message arrived from the driver
 handle_info({Port, {'L3L4m', CtrlBin, _DataBin}}, statename, {Module, StateName, StateData}) 
 			when is_binary(CtrlBin), size(CtrlBin) > 0 ->
